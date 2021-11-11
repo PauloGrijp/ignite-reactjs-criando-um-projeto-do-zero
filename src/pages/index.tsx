2b134146922e0a17e316dc8a7bebe9b48a84f1bc
Paulo Xavier <pxavierf@gmail.com>
2021-11-11 01:44:43 -0300

    renderizando lista de posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -36,7 +36,7 @@ export default function Home({ postsPagination }: HomeProps): ReactElement {
       ...post,
       first_publication_date: format(
         new Date(post.first_publication_date),
-        'dd MM YYYY',
+        'dd MMM yyyy',
         {
           locale: ptBR,
         }
@@ -45,45 +45,31 @@ export default function Home({ postsPagination }: HomeProps): ReactElement {
   });
 
   const [posts, setPosts] = useState(formattedDatePost);
-  console.log(posts);
+
   return (
     <>
       <main className={commonStyles.container}>
         <Header />
 
         <div className={styles.posts}>
-          <Link href="/">
-            <a className={styles.post}>
-              <h3>Título</h3>
-              <p>Subtítulo: Descrição do subtitulo</p>
-              <ul>
-                <li>
-                  <FiCalendar />
-                  25 mai 2021
-                </li>
-                <li>
-                  <FiUser />
-                  Paulo Xavier
-                </li>
-              </ul>
-            </a>
-          </Link>
-          <Link href="/">
-            <a className={styles.post}>
-              <h3>Título</h3>
-              <p>Subtítulo: Descrição do subtitulo</p>
-              <ul>
-                <li>
-                  <FiCalendar />
-                  25 mai 2021
-                </li>
-                <li>
-                  <FiUser />
-                  Paulo Xavier
-                </li>
-              </ul>
-            </a>
-          </Link>
+          {posts.map(post => (
+            <Link href={`/post/${post.uid}`} key={post.uid}>
+              <a className={styles.post}>
+                <h1>{post.data.title}</h1>
+                <p>{post.data.subtitle}</p>
+                <ul>
+                  <li>
+                    <FiCalendar />
+                    {post.first_publication_date}
+                  </li>
+                  <li>
+                    <FiUser />
+                    {post.data.author}
+                  </li>
+                </ul>
+              </a>
+            </Link>
+          ))}
           <button type="button">Carregar mais posts</button>
         </div>
       </main>
@@ -110,12 +96,12 @@ export const getStaticProps: GetStaticProps = async () => {
     };
   });
 
-  const postsPaginations = {
+  const postsPagination = {
     next_page: postsResponse.next_page,
     results: posts,
   };
 
   return {
-    props: { postsPaginations },
+    props: { postsPagination },
   };
 };
